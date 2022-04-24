95f87fbfa919dd479ed13bb801ea85649121f033
Asanka Gamage <asankagamage1998@gmail.com>
2022-04-24 13:58:55 +0530

    edited java files

--- src/com/billManagement/Bill.java
+++ src/com/billManagement/Bill.java
@@ -1,5 +1,18 @@
 package com.billManagement;
 
-public class Bill {
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType; 
+@Path("/Asanka") 
+public class Bill 
+{ 
+@GET
+@Path("/") 
+@Produces(MediaType.TEXT_PLAIN) 
+public String hello() 
+ { 
+ return "Hello world."; 
+ } 
+} 
 
-}
