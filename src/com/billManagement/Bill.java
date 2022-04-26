f254ce90e74195c16a6c5844797dd54fc50e0f19
Asanka Gamage <asankagamage1998@gmail.com>
2022-04-26 14:40:06 +0530

    created get,update and delete part

--- src/com/billManagement/Bill.java
+++ src/com/billManagement/Bill.java
@@ -81,6 +81,173 @@ public class Bill {
             } 
         return output; 
     } 
+    /**
+     * Get data User and bill database
+     * @return
+     */
+    public String readBill() 
+    { 
+    String output = ""; 
+    try
+    { 
+        Connection con = connect(); 
+        if (con == null) 
+        {return "Error while connecting to the database for reading."; } 
+        // Prepare the html table to be displayed
+        output = "<table border='1'><tr>"
+        		+ "<th>Bill ID</th>"
+        		+ "<th>Account ID</th>" +
+                "<th>Join Date</th>" + 
+                "<th>Meter Reding Before Date</th>"+
+                "<th>Meter Reding Before</th>" + 
+                "<th>Meter ReadingNowDate</th>" +
+                "<th>Meter ReadingNow</th>" + 
+                "<th>No Of UntitsConsumed</th>"+
+                "<th>Charge for electricity Consum</th>" + 
+                "<th>adjustments</th>" +
+                "<th>totalAmountDue</th>" + 
+                "<th>billDate</th>"+
+                "<th>Customer name</th></tr>" ; 
+        
+        /**Use join query connect tree table */
+        String query = "SELECT * FROM bill INNER JOIN users ON users.accountId = bill.accountId"; 
+        Statement stmt = con.createStatement(); 
+        ResultSet rs = stmt.executeQuery(query); 
+        // iterate through the rows in the result set
+        while (rs.next()) 
+        { 
+            String billId = rs.getString("billId"); 
+            String accountId = rs.getString("accountId"); 
+            String joinDate = rs.getString("joinDate"); 
+            String meterReadingBeforeDate = rs.getString("meterReadingBeforeDate"); 
+            String meterReadingBefore = rs.getString("meterReadingBefore"); 
+            String meterReadingNowDate = rs.getString("meterReadingNowDate"); 
+            String meterReadingNow = rs.getString("meterReadingNow"); 
+            String noOfUntitsConsumed = rs.getString("noOfUntitsConsumed"); 
+            String chargeforelectricityConsume = rs.getString("chargeforelectricityConsume");
+            String adjustments  = rs.getString("adjustments"); 
+            String totalAmountDue  = rs.getString("totalAmountDue"); 
+            String billDate  = rs.getString("billDate"); 
+            String CoustomerName  = rs.getString("firstName"); 
+            // Add into the html table
+            output += "<tr><td>" + billId + "</td>"; 
+            output += "<td>" + accountId + "</td>"; 
+            output += "<td>" + joinDate + "</td>"; 
+            output += "<td>" + meterReadingBeforeDate + "</td>"; 
+            output += "<td>" + meterReadingBefore + "</td>"; 
+            output += "<td>" + meterReadingNowDate + "</td>"; 
+            output += "<td>" + meterReadingNow + "</td>"; 
+            output += "<td>" + noOfUntitsConsumed + "</td>";
+            output += "<td>" + chargeforelectricityConsume + "</td>"; 
+            output += "<td>" + adjustments + "</td>"; 
+            output += "<td>" + totalAmountDue + "</td>"; 
+            output += "<td>" + billDate + "</td>"; 
+            output += "<td>" + CoustomerName + "</td></tr>";
+        } 
+        con.close(); 
+        // Complete the html table
+        output += "</table>"; 
+    } 
+	catch (Exception e) 
+	{ 
+		output = "Error while reading the Bills."; 
+		System.err.println(e.getMessage()); 
+	} 
+	return output; 
+	} 
+    
+   
+    /**
+     * Update Bill Information
+     * @param BillId
+     * @param accountId
+     * @param joinDate
+     * @param meterReadingBeforeDate
+     * @param meterReadingBefore
+     * @param meterReadingNowDate
+     * @param meterReadingNow
+     * @param noOfUntitsConsumed
+     * @param chargeforelectricityConsume
+     * @param adjustments
+     * @param totalAmountDue
+     * @param billDate
+     * @return
+     */
+    public String updateBill(String billId, String accountId, String joinDate, String meterReadingBeforeDate,
+    String meterReadingBefore, String meterReadingNowDate, String meterReadingNow, String noOfUntitsConsumed, String chargeforelectricityConsume,
+            String adjustments, Double totalAmountDue, String billDate )  
+    
+    { 
+        String output = ""; 
+    try
+    { 
+        Connection con = connect(); 
+        if (con == null) 
+    { 
+	return "Error while connecting to the database for updating."; } 
+    // create a prepared statement
+    String query = "UPDATE bill SET billId=?,accountId=?,joinDate=?,meterReadingBeforeDate=?"
+    		+ ",meterReadingBefore=?,meterReadingNowDate=?,meterReadingNow=?,noOfUntitsConsumed=?,"
+    		+ "chargeforelectricityConsume=? ,adjustments=?,totalAmountDue=?,billDate=? WHERE billId=?"; 
+    PreparedStatement preparedStmt = con.prepareStatement(query); 
+        // binding values
+        preparedStmt.setString(1, billId); 
+        preparedStmt.setString(2, accountId); 
+        preparedStmt.setString(3, joinDate); 
+        preparedStmt.setString(4, meterReadingBeforeDate);
+        preparedStmt.setString(5, meterReadingBefore); 
+        preparedStmt.setString(6, meterReadingNowDate);
+        preparedStmt.setString(7, meterReadingNow); 
+        preparedStmt.setString(8, noOfUntitsConsumed);
+        preparedStmt.setString(9, chargeforelectricityConsume); 
+        preparedStmt.setString(10, adjustments);
+        preparedStmt.setDouble(11, totalAmountDue );
+        preparedStmt.setString(12, billDate ); 
+        preparedStmt.setString(13, billId);
+    // execute the statement
+    preparedStmt.execute(); 
+    con.close(); 
+    output = "Updated successfully"; 
+    } 
+        catch (Exception e) 
+    { 
+        output = "Error while updating the Bill."; 
+        System.err.println(e.getMessage()); 
+    } 
+        return output; 
+    } 
+
+    
+    /**
+     * remove the Bill details from the table
+     * @param BillId
+     * @return
+     */
+    public String deleteBill(String BillId) 
+    { 
+	    String output = ""; 
+	    try
+	    { 
+	    	Connection con = connect(); 
+		    if (con == null) 
+		    	{return "Error while connecting to the database for deleting."; } 
+			    // create a prepared statement
+			    String query = "delete from bill where BillId=?"; 
+			    PreparedStatement preparedStmt = con.prepareStatement(query); 
+			    // binding values
+			    preparedStmt.setString(1, BillId); 
+			    // execute the statement
+			    preparedStmt.execute(); 
+			    con.close(); 
+			    output = "Deleted successfully"; 
+		    } 
+	    catch (Exception e) 
+	    { 
+		    output = "Error while deleting the Bill."; 
+		    System.err.println(e.getMessage()); 
+	    } 
+	    return output; 
+	    }
 
 }
 
--- src/com/billManagement/BillServices.java
+++ src/com/billManagement/BillServices.java
@@ -37,4 +37,51 @@ public class BillServices {
 	return output; 
 	}
 
+	//Get Bill details 
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String getBillDetails() 
+	{ 
+		return bill.readBill(); 
+	}
+	
+	//update bill information
+	@PUT
+	@Path("/Update") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateBill(String billData) 
+	{ 
+	//Convert the input string to a JSON object 
+	 JsonObject billObject = new JsonParser().parse(billData).getAsJsonObject(); 
+	//Read the values from the JSON object
+	  
+	 String billId   = billObject.get("billId").getAsString();
+	 String accountId  = billObject.get("accountId").getAsString(); 
+	 String joinDate  = billObject.get("joinDate").getAsString(); 
+	 String meterReadingBeforeDate  = billObject.get("meterReadingBeforeDate").getAsString(); 
+	 String meterReadingBefore  = billObject.get("meterReadingBefore").getAsString(); 
+	 String meterReadingNowDate  = billObject.get("meterReadingNowDate").getAsString(); 
+	 String meterReadingNow  = billObject.get("meterReadingNow").getAsString(); 
+	 String noOfUntitsConsumed  = billObject.get("noOfUntitsConsumed").getAsString(); 
+	 String chargeforelectricityConsume   = billObject.get("chargeforelectricityConsume").getAsString(); 
+	 String adjustments  = billObject.get("adjustments").getAsString(); 
+	 Double totalAmountDue  = billObject.get("totalAmountDue").getAsDouble(); 
+	 String billDate  = billObject.get("billDate").getAsString(); 
+	 
+	 String output = bill.updateBill(billId, accountId, joinDate, meterReadingBeforeDate, meterReadingBefore, meterReadingNowDate, meterReadingNow, noOfUntitsConsumed ,chargeforelectricityConsume, adjustments, totalAmountDue, billDate);
+	return output; 
+	
+	}
+	
+	//Remove the Bill
+	@DELETE
+	@Path("/Delete/{billId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePayment(@PathParam("billId") String billId) {
+		String response = bill.deleteBill(billId);
+		return response;
+	}
+
 }
