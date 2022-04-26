91b825e212c99f6fe829da3334212e912fda5db7
ShashiKrishan <vshashikrishan@yahoo.com>
2022-04-26 16:08:24 +0530

    CRUD Operations done without errors

--- src/com/NotificationManagement/Notification.java
+++ src/com/NotificationManagement/Notification.java
@@ -77,6 +77,145 @@ public class Notification {
             } 
         return output; 
     } 
+     /**
+     * Get data User and bill database
+     * @return
+     */
+    public String readNotification() 
+    { 
+    String output = ""; 
+    try
+    { 
+        Connection con = connect(); 
+        if (con == null) 
+        {return "Error while connecting to the database for reading."; } 
+        // Prepare the html table to be displayed
+        output = "<table border='1'><tr>"
+        		+ "<th>Notification ID</th>"
+        		+ "<th>Account ID</th>" +
+                "<th>Bill Id</th>" + 
+                "<th>Amount To Be a paid</th>"+
+                "<th>Email</th>" + 
+                "<th>Mobile Number</th>" +
+                "<th>Subject</th>" + 
+                "<th>Massage</th>"+
+                "<th>Date of Notify</th></tr>" ; 
+        
+        /**Use join query connect tree table */
+        String query = "SELECT * FROM  notification INNER JOIN users ON users.accountId = notification.accountId"; 
+        Statement stmt = con.createStatement(); 
+        ResultSet rs = stmt.executeQuery(query); 
+        // iterate through the rows in the result set
+        while (rs.next()) 
+        { 
+            String notificationId  = rs.getString("notificationId"); 
+            String accountId  = rs.getString("accountId"); 
+            String billId  = rs.getString("joinDate"); 
+            Double amountToBePaid  = rs.getDouble("amountToBePaid"); 
+            String email  = rs.getString("email"); 
+            String mobileNumber  = rs.getString("mobileNumber"); 
+            String subject  = rs.getString("subject"); 
+            String massage  = rs.getString("massage"); 
+            String dateNotify  = rs.getString("dateNotify");
+            String firstName  = rs.getString("firstName"); 
+            String nic = rs.getString("nic"); 
+            
+            // Add into the html table
+            output += "<tr><td>" + notificationId + "</td>"; 
+            output += "<td>" + accountId + "</td>"; 
+            output += "<td>" + billId + "</td>"; 
+            output += "<td>" + amountToBePaid + "</td>"; 
+            output += "<td>" + email + "</td>"; 
+            output += "<td>" + mobileNumber + "</td>"; 
+            output += "<td>" + subject + "</td>"; 
+            output += "<td>" + massage + "</td>";
+            output += "<td>" + dateNotify + "</td>"; 
+            output += "<td>" + firstName + "</td>";  
+            output += "<td>" + nic + "</td></tr>";
+        } 
+        con.close(); 
+        // Complete the html table
+        output += "</table>"; 
+    } 
+	catch (Exception e) 
+	{ 
+		output = "Error while reading the Notification."; 
+		System.err.println(e.getMessage()); 
+	} 
+	return output; 
+	} 
+    
+   
+   
+    public String updateNotification(String notificationId , String accountId , String billId , Double amountToBePaid ,
+    String email , String mobileNumber , String subject , String massage , String dateNotify )  
+    
+    { 
+        String output = ""; 
+    try
+    { 
+        Connection con = connect(); 
+        if (con == null) 
+    { 
+	return "Error while connecting to the database for updating."; } 
+    // create a prepared statement
+    String query = "UPDATE notification SET notificationId=?,accountId=?,billId=?,amountToBePaid=?,email=?,mobileNumber=?,subject=?,massage=?,dateNotify=? WHERE notificationId=?"; 
+    PreparedStatement preparedStmt = con.prepareStatement(query); 
+        // binding values
+        preparedStmt.setString(1, notificationId); 
+        preparedStmt.setString(2, accountId); 
+        preparedStmt.setString(3, billId); 
+        preparedStmt.setDouble(4, amountToBePaid);
+        preparedStmt.setString(5, email); 
+        preparedStmt.setString(6, mobileNumber);
+        preparedStmt.setString(7, subject); 
+        preparedStmt.setString(8, massage);
+        preparedStmt.setString(9, dateNotify); 
+        preparedStmt.setString(10, notificationId);
+      
+    // execute the statement
+    preparedStmt.execute(); 
+    con.close(); 
+    output = "Updated successfully"; 
+    } 
+        catch (Exception e) 
+    { 
+        output = "Error while updating the Notification."; 
+        System.err.println(e.getMessage()); 
+    } 
+        return output; 
+    } 
+
+    
+    /**
+     * remove the Notification details from the table
+     * @param BillId
+     * @return
+     */
+    public String deleteNotice(String BillId) 
+    { 
+	    String output = ""; 
+	    try
+	    { 
+	    	Connection con = connect(); 
+		    if (con == null) 
+		    	{return "Error while connecting to the database for deleting."; } 
+			    // create a prepared statement
+			    String query = "delete from notification where notificationId=?"; 
+			    PreparedStatement preparedStmt = con.prepareStatement(query); 
+			    // binding values
+			    preparedStmt.setString(1, BillId); 
+			    // execute the statement
+			    preparedStmt.execute(); 
+			    con.close(); 
+			    output = "Deleted successfully"; 
+		    } 
+	    catch (Exception e) 
+	    { 
+		    output = "Error while deleting the Notification."; 
+		    System.err.println(e.getMessage()); 
+	    } 
+	    return output; 
+	    }
 
 }
-
--- src/com/NotificationManagement/NotificationServices.java
+++ src/com/NotificationManagement/NotificationServices.java
@@ -35,5 +35,47 @@ public class NotificationServices {
 	return output; 
 	}
 
+	//Get Notification details 
+		@GET
+		@Path("/") 
+		@Produces(MediaType.TEXT_HTML) 
+		public String getBillDetails() 
+		{ 
+			return notification.readNotification(); 
+		}
+		
+		//update Notification information
+		@PUT
+		@Path("/Update") 
+		@Consumes(MediaType.APPLICATION_JSON) 
+		@Produces(MediaType.TEXT_PLAIN) 
+		public String updateNotice(String notice) 
+		{ 
+		//Convert the input string to a JSON object 
+		 JsonObject noticObject = new JsonParser().parse(notice).getAsJsonObject(); 
+		//Read the values from the JSON object
+		  
+		 String notificationId   = noticObject.get("notificationId").getAsString();
+		 String accountId  = noticObject.get("accountId").getAsString(); 
+		 String billId   = noticObject.get("billId").getAsString(); 
+		 Double amountToBePaid   = noticObject.get("amountToBePaid").getAsDouble(); 
+		 String email   = noticObject.get("email").getAsString(); 
+		 String mobileNumber   = noticObject.get("mobileNumber").getAsString(); 
+		 String subject  = noticObject.get("subject").getAsString(); 
+		 String massage   = noticObject.get("massage").getAsString(); 
+		 String dateNotify    = noticObject.get("dateNotify").getAsString(); 
+		
+		 String output = notification.updateNotification(notificationId, accountId, billId, amountToBePaid, email, mobileNumber, subject, massage, dateNotify);
+		return output; 
+		
+		}
+		
+		//Remove the Notification Id
+		@DELETE
+		@Path("/Delete/{noticeId}")
+		@Produces(MediaType.TEXT_PLAIN)
+		public String deleteNotice(@PathParam("noticeId") String noticeId) {
+			String response = notification.deleteNotice(noticeId);
+			return response;
+		}
 }
-
